6e7f96209c9257316452b7808a39eee3bc8a00b9
YJC <y3332664073@gmail.com>
2023-08-14 01:42:54 +0800

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,6 @@
+echo "memos一键更新脚本"
+echo "脚本作者：知性"
+echo "GitHub开源地址：https://github.com/aizhiqian/memos-on-replit"
+rm -rf memos && wget -O memos https://github.com/aizhiqian/halo-on-replit/releases/latest/download/memos
+echo "更新完毕！"
+echo "点击Run按钮重新启动本项目！！！"

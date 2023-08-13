24fdddcc55f81fe8d3fc30107a1071100fcbdb1e
YJC <y3332664073@gmail.com>
2023-08-14 01:41:06 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+echo "memos一键搭建脚本"
+echo "脚本作者：知性"
+echo "GitHub开源地址：https://github.com/aizhiqian/memos-on-replit"
+nix-env -iA nixpkgs.wget
+mkdir build
+cd build
+wget -O .replit https://raw.githubusercontent.com/aizhiqian/memos-on-replit/main/.replit
+wget -O replit.nix https://raw.githubusercontent.com/aizhiqian/memos-on-replit/main/replit.nix
+cd ..
+wget -O main.sh https://raw.githubusercontent.com/aizhiqian/memos-on-replit/main/main.sh
+wget -O memos https://github.com/aizhiqian/memos-on-replit/releases/download/0.14.4/memos
+cp -r build/.replit . && cp -r build/replit.nix .
+rm -rf build/
+echo "恭喜搭建完成"
+echo "点击Run立即运行项目"
+rm -rf README.md

6e96de3129ff16947177aebb8054bd54037ae92f
YJC <y3332664073@gmail.com>
2023-10-22 23:20:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ wget -O .replit https://raw.githubusercontent.com/aizhiqian/memos-on-replit/main
 wget -O replit.nix https://raw.githubusercontent.com/aizhiqian/memos-on-replit/main/replit.nix
 cd ..
 wget -O main.sh https://raw.githubusercontent.com/aizhiqian/memos-on-replit/main/main.sh
+wget -O update.sh https://raw.githubusercontent.com/aizhiqian/memos-on-replit/main/update.sh
 wget -O memos https://github.com/aizhiqian/memos-on-replit/releases/latest/download/memos
 cp -r build/.replit . && cp -r build/replit.nix .
 rm -rf build/
 echo "恭喜搭建完成"
 echo "点击Run立即运行项目"
-rm -rf README.md

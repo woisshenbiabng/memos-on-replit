598644a6df6653c088d64f3da0b90516509b936b
woisshenbiabng <168738629+woisshenbiabng@users.noreply.github.com>
2024-05-05 18:46:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 echo "memos一键搭建脚本"
 echo "脚本作者：知性"
-echo "GitHub开源地址：https://github.com/aizhiqian/memos-on-replit"
+echo "GitHub开源地址：https://github.com/woisshenbiabng/memos-on-replit"
 nix-env -iA nixpkgs.wget
 mkdir build
 cd build
-wget -O .replit https://raw.githubusercontent.com/aizhiqian/memos-on-replit/main/.replit
-wget -O replit.nix https://raw.githubusercontent.com/aizhiqian/memos-on-replit/main/replit.nix
+wget -O .replit https://raw.githubusercontent.com/woisshenbiabng/memos-on-replit/main/.replit
+wget -O replit.nix https://raw.githubusercontent.com/woisshenbiabng/memos-on-replit/main/replit.nix
 cd ..
-wget -O main.sh https://raw.githubusercontent.com/aizhiqian/memos-on-replit/main/main.sh
-wget -O update.sh https://raw.githubusercontent.com/aizhiqian/memos-on-replit/main/update.sh
-wget -O memos https://github.com/aizhiqian/memos-on-replit/releases/latest/download/memos
+wget -O main.sh https://raw.githubusercontent.com/woisshenbiabng/memos-on-replit/main/main.sh
+wget -O update.sh https://raw.githubusercontent.com/woisshenbiabng/memos-on-replit/main/update.sh
+wget -O memos https://github.com/woisshenbiabng/memos-on-replit/releases/latest/download/memos
 cp -r build/.replit . && cp -r build/replit.nix .
 rm -rf build/
 echo "恭喜搭建完成"
